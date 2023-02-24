76552e2607b894bd89a82ebcd4a2e30ce127499c
ABEL K ANTONY <abelkantony98@gmail.com>
2023-02-24 11:05:44 +0000

    update

--- .ccls-cache/@home@runner@2D-Array/main.cpp
+++ .ccls-cache/@home@runner@2D-Array/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+//function to search
+// void Search(int (&array)[])
+// {
+// 	int choice;
+// 	cout<<"\n\n1. SEARCH BY INDEX NUMBER";
+// 	cout<<"\n2. SEARCH BY NUMBER";
+// 	cout<<"\nENTER YOUR CHOICE : ";
+}
+
 int main() 
 {
 	srand(time(0));
+	int choice;
 	int x,y;
 	int lowerLimit = -100;
 	int upperLimit = 100;
@@ -28,5 +38,16 @@ int main()
 		}
 		cout<<endl;
 	}
-	
+	cout<<"\n\nOPTIONS";
+	cout<<"\n1. SEARCH";
+	cout<<"\nENTER YOUR CHOICE : ";
+	cin>>choice;
+	switch(choice)
+	{
+		case 1:
+		Search(array);
+		break;
+		default:
+		cout<<"\nINVALID OPTION";
+	}
 }
